5c6b41e8196302052e25fb1faf770991da86089d
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2025-09-10 13:21:04 -0700

    setting up the base test -> @BeforeClass && @AfterClass

---
+++ src/test/java/com/saucedemo/BaseTest.java
@@ -0,0 +1,39 @@
+package com.saucedemo;
+
+import com.saucedemo.pages.BasePage;
+import com.saucedemo.pages.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+    protected BasePage basePage;
+    protected LoginPage loginPage;
+    private String baseUrl = "https://www.saucedemo.com/v1/";
+
+    @BeforeClass
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--start-maximized");
+
+        driver = new ChromeDriver(options);
+
+        basePage = new BasePage();
+        basePage.setDriver(driver);
+
+        loginPage = new LoginPage();
+
+        driver.get(baseUrl);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
