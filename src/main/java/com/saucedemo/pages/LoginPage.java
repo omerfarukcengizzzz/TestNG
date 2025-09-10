4fff0d5f74bbd88af3a81d2042572bc149d101df
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2025-09-10 13:02:45 -0700

    added locator and login() method

--- src/main/java/com/saucedemo/pages/LoginPage.java
+++ src/main/java/com/saucedemo/pages/LoginPage.java
@@ -1,4 +1,24 @@
 package com.saucedemo.pages;
 
-public class LoginPage {
+import org.openqa.selenium.By;
+
+public class LoginPage extends BasePage {
+
+    private By username = By.id("user-name");
+    private By password = By.id("password");
+    private By loginButton = By.id("login-button");
+    private By errorMessage = By.xpath("//h3[@data-test='error']");
+
+    public ProductsPage login(String username, String password) {
+        set(this.username, username);
+        set(this.password, password);
+        click(this.loginButton);
+
+        return new ProductsPage();
+    }
+
+    public String getErrorMessage() {
+        return find(errorMessage).getText();
+    }
+
 }
